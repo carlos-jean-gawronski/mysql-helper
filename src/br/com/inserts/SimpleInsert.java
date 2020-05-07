dab804b455224a45cc4dd0fb8a580081e703b3dc
Carlos Jean Gawronski <cereby222plays@gmail.com>
2020-05-06 22:14:46 -0300

    :sparkles: adding simple insert method

--- src/br/com/inserts/SimpleInsert.java
+++ src/br/com/inserts/SimpleInsert.java
@@ -5,26 +5,56 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import br.com.main.ConnectionDB;
+
 public class SimpleInsert {
-
-	public SimpleInsert(Connection dbConnection) {
-		this.setCon(dbConnection);
-	}
+	
+	private ConnectionDB conn = new ConnectionDB();
 	private Connection con;
-	public PreparedStatement ps;
-	public ResultSet rs;
-	public String sql;
-	
-	public Connection getCon() {
-		return con;
-	}
-	
-	public void setCon(Connection con) {
-		this.con = con;
-	}
+	private PreparedStatement ps;
+	private ResultSet rs;
+	private String sql;
 
-	public void insert(String TABLE) throws SQLException{
-		sql = "insert into ? ";
+	public String insert(String TABLE, String DB_NAME, String DB_USER, String DB_PASSWORD, String[] fields, String[] values){
+		try {
+			con = conn.openConnectionDatabase(DB_NAME, DB_USER, DB_PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
+		if(values == null) {
+			return "Missing values field";
+		}
+		
+		String val = "";
+		for(int i = 0; i < values.length; i++) {
+			if (i == (values.length - 1)) {
+				val += values[i];
+			} else {
+				
+				val += values[i] + ", ";
+			}
+		}
+		if(fields == null) {
+			sql = "insert into " + TABLE + " values (" + val + ")";
+		} else {
+			String flds = "";
+			for(int i = 0; i < fields.length; i++) {
+				if (i == (fields.length - 1)) {
+					flds += fields[i];
+				} else {
+					
+					flds += fields[i] + ", ";
+				}
+			}
+			sql = "insert into " + TABLE + " (" + flds + ") values (" + val + ")";
+		}
+		try {
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "Success inserting into table " + TABLE;
 	}
 }
